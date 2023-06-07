2ae7585794c14a3b3c09d7d0479925c913e86171
Maxim <toper.one111@gmail.com>
2023-06-07 17:52:18 +0300

    Check for a valid number of arguments

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,11 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
+  if (argc != 2)
+  {
+    cerr << "Usage: " << argv[0] << " <filepath>" << endl;
+    return 1;
+  }
   string filename(argv[1]);
   fs::path path{filename};
   try
